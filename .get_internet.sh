dab4a30dd6cf48630d0acdaa24c2f350d6b4871f
sams25 <anindya.sharma1@gmail.com>
2018-10-19 12:43:53 +0100

    prevented creation of downloaded webpages

--- .get_internet.sh
+++ .get_internet.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+
 # the success code is 0 (Unix standards)
-wifi_success="$(wget -q g.gl ; echo $?)"
+wifi_success="$(wget -q g.gl -O /dev/null; echo $?)"
 
 if [ "${wifi_success}" == "0" ]
 then
